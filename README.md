8eea0d0bfb3b90136aca78a89e0a340e5e5991c4
Mia Tangvik <mita.94@hotmail.com>
2018-05-29 19:37:01 +0200

    Write README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Tema 14
+# Powergum
+
+School project made for our graduation exhibition.
+
+## Installing
+
+Run
+
+```
+npm install
+```
+
+to install dependencies.
+
+Secondly run
+
+```
+npm start
+```
+
+Go to
+
+```
+localhost:3000
+```
+
+and Powergum should be good to go!
+
+## Optional
+
+If you have an Arduino board, it is possible to use a button connected with your board to "eject" a gum. You need to have StandardFirmataPlus installed on your board.
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var server = app.listen(3000);
 app.use(express.static('public'));
 
 //  Johnny-Five
-var five  = require('johnny-five');
+var five = require('johnny-five');
 var board = new five.Board();
 
 // Socket.io
@@ -15,19 +15,14 @@ var socket = require('socket.io');
 var io = socket(server);
 
 // New socket connection
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
     console.log('New connection ' + socket.id);
 });
 
 board.on('ready', function () {
-    // Create a new `button` hardware instance.
-    // This example allows the button module to
-    // create a completely default instance
     button = new five.Button(2);
 
-    // Inject the `button` hardware into
-    // the Repl instance's context;
-    // allows direct command line access
+    // Allow direct command line access
     board.repl.inject({
         button: button
     });
